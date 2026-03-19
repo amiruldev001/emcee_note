9519f503d6826e76e6d5f2f116a7bfcabfb2c5e2
amiruldev001 <amirul.dev001@gmail.com>
2026-03-19 23:13:03 +0800

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ function calculate() {
     let rate = parseFloat(document.getElementById("rate").value) / 100;
     let years = parseInt(document.getElementById("years").value);
 
+    let type = document.querySelector('input[name="interestType"]:checked').value;
+
     if (!price || !dpPercent || !rate) {
         alert("Isi semua maklumat");
         return;
@@ -12,13 +14,28 @@ function calculate() {
     let dp = price * (dpPercent / 100);
     let loan = price - dp;
     let months = years * 12;
-    let monthlyRate = rate / 12;
 
-    let monthly = loan * (monthlyRate * Math.pow(1 + monthlyRate, months)) /
-        (Math.pow(1 + monthlyRate, months) - 1);
+    let monthly, total, interest;
 
-    let total = monthly * months;
-    let interest = total - loan;
+    if (type === "reducing") {
+        let monthlyRate = rate / 12;
+
+        monthly = loan * (monthlyRate * Math.pow(1 + monthlyRate, months)) /
+            (Math.pow(1 + monthlyRate, months) - 1);
+
+        total = monthly * months;
+        interest = total - loan;
+
+        generateScheduleReducing(loan, monthly, monthlyRate, months);
+
+    } else {
+        // FLAT RATE
+        interest = loan * rate * years;
+        total = loan + interest;
+        monthly = total / months;
+
+        generateScheduleFlat(loan, monthly, interest, months);
+    }
 
     document.getElementById("result").innerHTML = `
         Loan: RM ${loan.toFixed(2)}<br>
@@ -26,28 +43,4 @@ function calculate() {
         Total: RM ${total.toFixed(2)}<br>
         Interest: RM ${interest.toFixed(2)}
     `;
-
-    generateSchedule(loan, monthly, monthlyRate, months);
-}
-
-function generateSchedule(balance, monthly, rate, months) {
-    let tbody = document.querySelector("#schedule tbody");
-    tbody.innerHTML = "";
-
-    for (let i = 1; i <= months; i++) {
-        let interest = balance * rate;
-        let principal = monthly - interest;
-        balance -= principal;
-
-        let row = `
-            <tr>
-                <td>${i}</td>
-                <td>${monthly.toFixed(2)}</td>
-                <td>${interest.toFixed(2)}</td>
-                <td>${principal.toFixed(2)}</td>
-                <td>${balance > 0 ? balance.toFixed(2) : 0}</td>
-            </tr>
-        `;
-        tbody.innerHTML += row;
-    }
 }
